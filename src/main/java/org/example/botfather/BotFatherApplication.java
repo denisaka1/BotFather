f6067c3c6e1938aa4f1cf75639f6aed1afb4e830
denisaka1 <denis.karabitski@gmail.com>
2025-01-19 13:51:19 +0200

    Initial commit

---
+++ src/main/java/org/example/botfather/BotFatherApplication.java
@@ -0,0 +1,13 @@
+package org.example.botfather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotFatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotFatherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/botfather/BotFatherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.botfather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotFatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

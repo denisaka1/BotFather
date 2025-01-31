2e0c1ffedb82f2b8eb53265b17d25469476a0445
lidar <lidar602@gmail.com>
2025-01-30 17:39:47 -0800

    fix

--- src/main/java/org/example/botfather/config/ConfigLoader.java
+++ src/main/java/org/example/botfather/config/ConfigLoader.java
@@ -1,27 +1,15 @@
 package org.example.botfather.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import lombok.Getter;
+import lombok.Setter;
 
 @Configuration
 @ConfigurationProperties(prefix = "telegram.bot")
+@Getter
+@Setter
 public class ConfigLoader {
 
     private String username;
     private String token;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
 }

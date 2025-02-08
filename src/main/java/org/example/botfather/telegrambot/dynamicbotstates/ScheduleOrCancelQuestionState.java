3e1647a444db7ba93de94a959b069189751459d5
lidar <lidar602@gmail.com>
2025-02-08 14:24:36 -0800

    Fix

--- src/main/java/org/example/botfather/telegrambot/dynamicbotstates/ScheduleOrCancelQuestionState.java
+++ src/main/java/org/example/botfather/telegrambot/dynamicbotstates/ScheduleOrCancelQuestionState.java
@@ -5,7 +5,6 @@ import org.example.botfather.telegramform.ButtonsGenerator;
 import org.example.botfather.telegramform.MessageGenerator;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;

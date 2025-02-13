b62696c88a2c1ace8814f0c15839e4e570a76e36
denisaka1 <denis.karabitski@gmail.com>
2025-02-13 19:47:40 +0200

    Cleanup code

--- telegram-bot/src/main/java/org/example/telegram/bot/actions/manager/StartSlashCommand.java
+++ telegram-bot/src/main/java/org/example/telegram/bot/actions/manager/StartSlashCommand.java
@@ -3,18 +3,13 @@ package org.example.telegram.bot.actions.manager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.client.api.controller.BusinessOwnerApi;
-import org.example.data.layer.entities.BotCreationState;
 import org.example.data.layer.entities.BusinessOwner;
 import org.example.data.layer.entities.OwnerRegistrationState;
-import org.example.telegram.components.forms.FormStep;
-import org.example.telegram.components.forms.GenericForm;
 import org.example.telegram.components.validators.*;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.User;
 
-import java.util.Arrays;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
 
 @Slf4j
@@ -24,13 +19,10 @@ public class StartSlashCommand implements ISlashCommand {
 
     private final BusinessOwnerApi businessOwnerApi;
 
-    private GenericForm userForm;
     private BusinessOwner businessOwner;
 
     @Override
     public String execute(Message message) {
-//        createForm();
-
         User telegramUser = message.getFrom();
         BusinessOwner incomingOwner = BusinessOwner.builder()
                 .firstName(telegramUser.getFirstName())
@@ -46,7 +38,6 @@ public class StartSlashCommand implements ISlashCommand {
     }
 
     public String processUserResponse(Message message) {
-//        BotCreationState currentState = botSession.getCreationState();
         String userInput = message.getText();
         OwnerRegistrationState currentState = businessOwner.getRegistrationState();
 
@@ -67,9 +58,6 @@ public class StartSlashCommand implements ISlashCommand {
             response.set(nextState.getMessage());
         }, () -> {
             // If no next state, user has completed registration
-//            botSessionService.finalizeBotSession(userId, botSession);
-//            businessOwnerApi.create(userId, businessOwner);
-            response.set("🎉 Your new bot has been created successfully!\nYou can now access it using the link from the first message.\n\n🙏 Thank you for creating new bot with us! Type any text to continue.");
         });
         return response.get();
     }
@@ -81,13 +69,5 @@ public class StartSlashCommand implements ISlashCommand {
             case ASK_ADDRESS -> new StringValidator().validate(userMessage);
             default -> true;
         };
-    }
-
-    private void createForm() {
-        userForm = new GenericForm(Arrays.asList(
-                new FormStep<>("📱 What is your phone number?", new PhoneNumberValidator(), "❌ Invalid phone number!", "✅ Phone number is saved.", "phoneNumber"),
-                new FormStep<>("📧 What is your email?", new EmailValidator(), "❌ Invalid email!", "✅ Email is saved.", "email"),
-                new FormStep<>("🏠 What is your address?", new StringValidator(), "❌ Invalid address!", "✅ Address is saved.", "address")
-        ), "👋 Welcome to the Users Creator!\n\nPlease follow all the instructions. You can go back anytime by typing /back.", "🎉 Thank you for registering! Type any text to continue.");
     }
 }

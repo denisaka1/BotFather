744569c5515d268d67b6962a1e356fe5dda7fcf6
lidar <lidar602@gmail.com>
2025-02-13 15:10:22 +0200

    fix client creation

--- client-api-layer/src/main/java/org/example/client/api/controller/ClientApi.java
+++ client-api-layer/src/main/java/org/example/client/api/controller/ClientApi.java
@@ -1,6 +1,7 @@
 package org.example.client.api.controller;
 import lombok.AllArgsConstructor;
 import org.example.client.api.helper.ApiRequestHelper;
+import org.example.data.layer.entities.Appointment;
 import org.example.data.layer.entities.Client;
 import org.springframework.stereotype.Component;
 
@@ -8,13 +9,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class ClientApi {
 
-    private static final String BASE_URL = "http://localhost:8080/api/client/";
+    private static final String BASE_URL = "http://localhost:8080/api/client";
 
     private final ApiRequestHelper apiRequestHelper;
 
     public Client getClient(Long userId) {
         return apiRequestHelper.get(
-                BASE_URL + userId,
+                BASE_URL + "/" + userId,
                 Client.class
         );
     }
@@ -24,6 +25,14 @@ public class ClientApi {
                 BASE_URL,
                 client,
                 Client.class
+        );
+    }
+
+    public Appointment createAppointment(Appointment appointment, Long clientId, Long botId) {
+        return apiRequestHelper.post(
+                BASE_URL + "/" + clientId + "/appointment",
+                appointment,
+                Appointment.class
         );
     }
 }
